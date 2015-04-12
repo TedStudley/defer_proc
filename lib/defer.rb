dfd7add2f8ef15ae33d05e4221f15daf51006739
tedstudley <tedstudley@gmail.com>
2015-04-11 22:18:25 -0700

    Added documentation

--- lib/defer.rb
+++ lib/defer.rb
@@ -5,7 +5,9 @@ class Defer
 
   # Initialize with the block for deferred execution
   def initialize(interval=0.1, &block)
+    # `Defer` requires a block be passed to initialization
     throw ArgumentError.new("Defer must be initialized with a block") if block.nil?
+    # Interval must be a positive number
     throw ArgumentError.new("Polling interval must be a number.") unless interval.is_a? Numeric
     throw ArgumentError.new("Polling interval must be positive.") unless interval > 0
 
@@ -15,21 +17,28 @@ class Defer
 
   # Defer execution until the provided block is satisfied
   def until(&conditional)
+    # `Defer#until` must be passed a conditional block
     throw ArgumentError.new("A conditional block is required") if conditional.nil?
+
+    # forward the (negated) block to `Defer#while`
     conditional_proc = Proc.new { not conditional.call }
     self.while(&conditional_proc)
   end
 
   # Defer execution of the @deferred_block while the provided block is satisfied
   def while(&conditional)
+    # `Defer#while` must be passed a conditional block
     throw ArgumentError.new("A conditional block is required") if conditional.nil?
+
     Thread.new {
       # Lower the priority for the polling thread
       Thread.current.priority = -2
 
       while yield do
+        # Poll ever `@interval` seconds until the conditional block is satisfied.
         sleep @interval
       end
+      # Call the deferred block
       @deferred_block.call
     }
   end
